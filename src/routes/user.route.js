a427855d524bc9ced7eae7fbfdbaa55d7ddf48b3
Hugo <hugoj.egu@gmail.com>
2019-11-13 16:30:09 +0100

    verify token get all

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -4,7 +4,7 @@ const user = require('../controllers/user.controller.js');
 const verifyToken = require ('../helpers/verifyToken');
 
 // Create a new User
-router.post('/users', verifyToken,user.create);
-router.get('/users', user.findAll);
+router.post('/users', user.create);
+router.get('/users', verifyToken,user.findAll);
 
 module.exports = router;
