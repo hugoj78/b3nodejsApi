4d724b8d32b992ce4ee10e8349222512b0684ca8
Hugo <hugoj.egu@gmail.com>
2019-11-19 12:24:18 +0100

    crud post

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,8 +3,10 @@ const router = express.Router();
 
 const userRouter = require('./user.route');
 const authRouter = require('./auth.route');
+const postRouter = require('./post.route');
 
 router.use(userRouter);
 router.use(authRouter);
+router.use(postRouter);
 
 module.exports = router;
