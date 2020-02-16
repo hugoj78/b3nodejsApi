4ee9481d2d91f28332f00ea8d5327b49fca88b12
wassim <x.liquid1@gmail.com>
2020-02-16 20:06:24 +0100

    fix user

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -5,6 +5,7 @@ const auth = require('../controllers/account.controller.js');
 
 router.post('/auth/register', auth.create);
 router.post('/auth/login', auth.login);
+router.get('/user/:id', auth.getAccount);
 
 
 module.exports = router;
